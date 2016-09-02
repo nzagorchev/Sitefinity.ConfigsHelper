d76db91c4bd9ffdfaea21d23237316a9bdca06c1
nzagorchev <nikola.zagorchev@gmail.com>
2016-09-02 15:28:59 +0300

    Initial commit

---
+++ Configurations/ConfigurationsHelper.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using Telerik.Sitefinity.Configuration;
+using Telerik.Sitefinity.Data;
+using Telerik.Sitefinity.Modules.Events;
+using Telerik.Sitefinity.Web.UI.Backend.Elements.Config;
+using Telerik.Sitefinity.Web.UI.Backend.Elements.Widgets;
+using Telerik.Sitefinity.Web.UI.ContentUI.Config;
+using Telerik.Sitefinity.Web.UI.ContentUI.Views.Backend.Master.Config;
+using Telerik.Sitefinity.Web.UI.Fields.Config;
+
+namespace SitefinityWebApp.Configurations
+{
+    public class ConfigurationsHelper
+    {
+        protected static T GetContentViewElement<T>(ConfigManager configManager, string contentViewControlName,
+    string viewName, out ContentViewConfig config)
+            where T : ContentViewDefinitionElement
+        {
+            config = configManager.GetSection<ContentViewConfig>();
+            var contentBackend = config.ContentViewControls[contentViewControlName];
+            var view = contentBackend.ViewsConfig.Values.FirstOrDefault(v => v.ViewName == viewName);
+            var viewElement = (T)view;
+
+            return viewElement;
+        }
+
+        protected static FieldDefinitionElement GetFieldElement(ContentViewDetailElement contentView, string sectionName, string fieldName)
+        {
+            var sectionElement = contentView.Sections[sectionName];
+            var fieldElement = sectionElement.Fields[fieldName];
+
+            return fieldElement;
+        }
+
+        protected static WidgetBarSectionElement GetToolbarElement(MasterGridViewElement masterView)
+        {
+            var toolbar = masterView.ToolbarConfig.Sections.Elements.Where(e => e.Name == "toolbar").First();
+
+            return toolbar;
+        }
+
+        public virtual void AddExtensionScript(MasterGridViewElement masterView,
+            string scriptLocation, string loadMethodName,
+            out bool needSave)
+        {
+            needSave = false;
+            var script = masterView.Scripts.Elements
+                .Where(e => e.ScriptLocation == scriptLocation)
+                .FirstOrDefault();
+
+            if (script == null)
+            {
+                var clientScript = new ClientScriptElement(masterView.Scripts);
+                clientScript.ScriptLocation = scriptLocation;
+                clientScript.LoadMethodName = loadMethodName;
+                clientScript.CollectionItemName = "script";
+                masterView.Scripts.Add(clientScript);
+
+                needSave = true;
+            }
+        }
+
+        protected virtual CommandWidgetElement CreateCommandWidgetElement(ActionMenuColumnElement parent,
+            string commandArgument, string commandName,
+            string name, string text)
+        {
+            var commandWidget = new CommandWidgetElement(parent.MenuItems);
+            commandWidget.CommandArgument = commandArgument;
+            commandWidget.CommandName = commandName;
+            commandWidget.Name = name;
+            commandWidget.Text = text;
+            commandWidget.WrapperTagKey = HtmlTextWriterTag.Li;
+            commandWidget.ButtonType = Telerik.Sitefinity.Web.UI.Backend.Elements.Enums.CommandButtonType.Standard;
+            commandWidget.WidgetType = typeof(CommandWidget);
+
+            return commandWidget;
+        }
+    }
+}
